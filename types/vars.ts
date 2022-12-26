01804f80697fdd6bc6592026905f23d9b612cf20
Kyrillus Mehanni <99624083+Kyrillus@users.noreply.github.com>
2022-12-27 00:15:30 +0100

    login v1

--- types/vars.ts
+++ types/vars.ts
@@ -1,11 +1,17 @@
+import { FcSettings } from 'react-icons/fc';
+import { MdContactPage } from 'react-icons/md';
+
 export const navItems = [
-    {name: "Dashboard", path:"/dashboard"},
-    {name:"Network", path: "/network"},
-    {name:"Teams", path: "/teams"},
-    {name:"Contact", path: "/contact"}
+    {name: "Dashboard", path: "/dashboard"},
+    {name: "Network", path: "/network"},
+    {name: "Teams", path: "/teams"},
+    {name: "Contact", path: "/contact"}
 ];
 
-export const profileMenu = ["Profile", "Settings"];
+export const profileSettings = [
+    {name: "profile", icon: MdContactPage},
+    {name: "settings", icon: FcSettings, size: 5},
+];
 
 export const moonColorDesktop = "#C9C6C6FF";
 export const sunColorDesktop = "#51EBF3";
