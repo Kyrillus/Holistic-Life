895a0c5b4a1d58d072b7c63bc1360635e66b6e59
Kyrillus Mehanni <99624083+Kyrillus@users.noreply.github.com>
2022-12-30 16:57:19 +0100

    mobile menu more responsive

--- components/navbar/mobileMenu.tsx
+++ components/navbar/mobileMenu.tsx
@@ -18,7 +18,7 @@ function MobileMenu({navbarOpen, setNavbarOpen}: { navbarOpen: any, setNavbarOpe
         }
     }, []);
 
-    useEffect( () => {
+    useEffect(() => {
         if (navbarOpen)
             document.body.style.overflow = "hidden"
         else
@@ -30,28 +30,31 @@ function MobileMenu({navbarOpen, setNavbarOpen}: { navbarOpen: any, setNavbarOpe
     return (
         <div>
             <nav
-                className={`select-none fixed flex top-0 left-0 w-full px-10 z-20 h-screen pt-32 overflow-y-hidden text-5xl bg-aero dark:bg-prussianBlue transform transition-all duration-500 ${
+                className={`select-none fixed flex top-0 left-0 w-full px-10 z-20 min-h-screen pt-28 overflow-y-hidden overflow-x-hidden text-4xl bg-aero dark:bg-prussianBlue transition-all transform duration-500 ${
                     navbarOpen ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-full"
                 }`}>
-                <ul className="w-full flex flex-col gap-8  text-center">
-                    {navItems.map( (item) =>
+                <ul className="w-full flex flex-col gap-2 text-center">
+                    {navItems.map((item) =>
                         <li
                             key={item.name}
-                            className="flex hover:cursor-pointer w-full hover:scale-110 leading-8 list-none focus:outline-none group py-2 tracking-normal opacity-100 transition-all duration-200 ease-in-out">
+                            className="flex hover:cursor-pointer w-full hover:scale-110 leading-8 list-none focus:outline-none group py-2 tracking-normal transition-transform opacity-100 duration-200 ease-in-out">
                             <a onClick={async () => {
                                 await router.push(item.path);
                                 setNavbarOpen(false);
-                            }} className="h-full w-full py-2  text-white">
+                            }} className="h-full w-full py-2 text-white">
                                 {item.name}
                             </a>
                         </li>
                     )}
-                    <div className="flex flex-col justify-center items-center self-center w-96 border-t border-t-2 mt-5">
+                    <div
+                        className="flex flex-col justify-center items-center self-center w-96 border-t border-t-2 mt-5">
                         <div className="pt-5">
                             <p onClick={async () => {
                                 await router.push("/login");
                                 setNavbarOpen(false);
-                            }} className="text-white border rounded-xl p-2 text-4xl border-4 border-white cursor-pointer">log in</p>
+                            }}
+                               className="text-white border rounded-xl py-2 px-8 text-3xl border border-white cursor-pointer">log
+                                in</p>
                         </div>
                         <div className="pt-5">
                             <LightDarkModeSwitch sunColor={sunColorMobile} moonColor={moonColorMobile} size={80}/>
