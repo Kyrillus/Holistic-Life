b2be05c8b707bc8b92ba6f55b43bf3ef5a498505
Kyrillus Mehanni <99624083+Kyrillus@users.noreply.github.com>
2023-01-03 13:11:29 +0100

    login oatuh credentials

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,33 @@
 import '../styles/globals.css'
-import type {AppProps} from 'next/app'
 import Head from "next/head";
 import Navbar from "../components/navbar/navbar";
 import Footer from "../components/footer/footer";
 import {ThemeProvider} from "next-themes";
 import {useState} from "react";
 import MobileMenu from "../components/navbar/mobileMenu";
+import {SessionProvider} from "next-auth/react"
+import {AppProps} from "next/app";
 
 function MyApp({Component, pageProps}: AppProps) {
     const [navbarOpen, setNavbarOpen] = useState(false);
 
     return (
         <ThemeProvider attribute="class" enableSystem={false}>
-            <Head>
-                <title>Holistic Life</title>
-                <link rel="icon"
-                      href=""
-                      sizes="32x32"/>
-                <link rel="icon"
-                      href=""
-                      sizes="192x192"/>
-            </Head>
-            <Navbar navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen}/>
-            <MobileMenu navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen}/>
-            <Component {...pageProps} />
-            <Footer/>
+            <SessionProvider session={pageProps.session}>
+                <Head>
+                    <title>Holistic Life</title>
+                    <link rel="icon"
+                          href=""
+                          sizes="32x32"/>
+                    <link rel="icon"
+                          href=""
+                          sizes="192x192"/>
+                </Head>
+                <Navbar navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen}/>
+                <MobileMenu navbarOpen={navbarOpen} setNavbarOpen={setNavbarOpen}/>
+                <Component {...pageProps} />
+                <Footer/>
+            </SessionProvider>
         </ThemeProvider>
     )
 }
