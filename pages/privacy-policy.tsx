ec5df8876f9866c75821012cb8376f06af9d1da4
Kyrillus Mehanni <99624083+Kyrillus@users.noreply.github.com>
2023-01-03 23:47:17 +0100

    policy text (without formatting)

--- pages/privacy-policy.tsx
+++ pages/privacy-policy.tsx
@@ -2,8 +2,66 @@ import React from 'react';
 
 function PrivacyPolicy() {
     return (
-        <div>
+        <div className="mx-auto px-14 py-10 w-1/2">
+            <p>
+                Privacy Policy
+                Effective Date: January 3, 2023
+                Welcome to our website, holistic-life.dev. Your privacy is important to us. This privacy policy ("Privacy Policy") explains how we collect, use, and share information about you when you visit or use the Site.
+                By using the Site, you agree to the collection, use, and sharing of your information as described in this Privacy Policy. If you do not agree with our policies and practices, do not use the Site.
+                This Privacy Policy may change from time to time. We encourage you to review the Privacy Policy whenever you access the Site to stay informed about our information practices and the choices available to you.
+                Information We Collect
+                We collect information from you in a variety of ways when you use the Site.
+                1.1 Information You Provide to Us
+                We collect information you provide directly to us. For example, when you sign up for our newsletter or contact us through the Site, we may collect your name, email address, and other contact information.
+                1.2 Information We Collect Automatically
+                When you use the Site, we may collect information about your device and your use of the Site. This information may include your IP address, browser type and version, and other device and usage information.
+                We may also use cookies, web beacons, and other tracking technologies to collect information about your use of the Site. You can control the use of cookies at the individual browser level, but if you choose to disable cookies, it may limit your use of certain features or functions on the Site.
+                How We Use Your Information
+                We use the information we collect to operate and improve the Site, to provide and personalize the services we offer, and to communicate with you.
+                2.1 To Operate and Improve the Site
+                We use the information we collect to operate and improve the Site, including to:
+                Provide and maintain the Site
+                Monitor and analyze usage and trends
+                Personalize the Site for you
+                2.2 To Provide and Personalize the Services We Offer
+                We use the information we collect to provide and personalize the services we offer, including to:
+                Respond to your inquiries and requests
+                Provide customer service
+                Send you newsletters, marketing materials, and other communications
+                2.3 To Communicate With You
+                We use the information we collect to communicate with you, including to:
+                Respond to your inquiries and requests
+                Send you newsletters, marketing materials, and other communications
+                How We Share Your Information
+                We may share your information with third parties in the following circumstances:
+                3.1 With Your Consent
+                We may share your information with third parties when you have provided your consent to do so.
+                3.2 With Service Providers
+                We may share your information with third-party service providers who perform services on our behalf, such as hosting and maintenance, marketing, and email and SMS messaging services.
+                3.3 For Legal Reasons
+                We may share your information if we believe it is necessary to comply with a legal obligation, such as a court order or subpoena, or to protect the safety, rights, or property of us or others.
+                Your Choices
+                You have the following choices regarding the information we collect and use:
+                4.1 Opting Out of Communications
+                You may opt out of receiving newsletters, marketing materials, and other communications from us by following the unsubscribe instructions included in
+                4.2 Accessing and Updating Your Information
+                You have the right to access and update the personal information you have provided to us. You may update your information by contacting us at [Insert contact email or method here].
+                4.3 Controlling Cookies
+                You can control the use of cookies at the individual browser level, but if you choose to disable cookies, it may limit your use of certain features or functions on the Site.
+                Data Security
+                We have implemented measures designed to secure your personal information from accidental loss and from unauthorized access, use, alteration, and disclosure. However, we cannot guarantee that unauthorized third parties will never be able to defeat those measures or use your personal information for improper purposes.
+                Data Retention
+                We will retain your personal information for as long as necessary to fulfill the purposes for which it was collected, including to comply with our legal obligations.
+                Children's Privacy
+                The Site is not intended for children under the age of 18. We do not knowingly collect personal information from children under 18. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us at [Insert contact email or method here].
+                Links to Other Websites
+                The Site may contain links to other websites that are not operated by us. If you click on a third-party link, you will be directed to that third party's site. We strongly advise you to review the privacy policy of every site you visit. We have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.
+                Changes to this Privacy Policy
+                We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page. You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.
+                Contact Us
+                If you have any questions about this Privacy Policy, please contact us at privacy@holistic-life.dev.
 
+            </p>
         </div>
     );
 }
