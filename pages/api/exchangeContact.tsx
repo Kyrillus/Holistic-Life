1bc9fb4945596c59580e2dd9df8632817398bb3a
Kyrillus Mehanni <99624083+Kyrillus@users.noreply.github.com>
2023-01-24 00:58:51 +0100

    change nodemailer settings

--- pages/api/exchangeContact.tsx
+++ pages/api/exchangeContact.tsx
@@ -6,13 +6,9 @@ export default async (req: any, res: any) => {
     const transporter = nodemailer.createTransport({
         host: 'smtp.mailersend.net',
         port: 587,
-        secure: false, // true for 465, false for other ports
         auth: {
             user: 'MS_2C85TY@holistic-life.dev',
             pass: 'EFj5aRHRihZ2WLhj'
-        },
-        tls:{
-            rejectUnauthorized:false
         }
     });
 
