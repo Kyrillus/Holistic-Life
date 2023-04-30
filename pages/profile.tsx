4c10aff9431de3d4dc890316e4b8827e4aa98078
Sebastian Fürndraht <sebastian.fuerndraht@gmail.com>
2023-04-30 17:00:26 +0200

    updated profile page to include mailerlist subscribe/unsubscribe feature

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -9,6 +9,7 @@ import {useSession} from "next-auth/react";
 import {updateUser, userDetails} from "../lib/userAPI";
 import {profileForms} from "../types/User/Profile";
 import LoadingSpinner from "../components/Loaders/LoadingSpinner";
+import {Switch} from "@chakra-ui/react";
 
 function Profile() {
     const {data, status} = useSession<any>();
@@ -22,7 +23,8 @@ function Profile() {
         birthday: new Date(),
         male: true,
         country: '',
-        city: ''
+        city: '',
+        mailerlist: true,
     })
 
     useEffect(() => {
@@ -36,7 +38,8 @@ function Profile() {
                     birthday: new Date(data.birthday != undefined ? data.birthday : new Date()),
                     male: data.male,
                     country: data.country,
-                    city: data.city
+                    city: data.city,
+                    mailerlist: data.mailerlist,
                 });
                 setDataLoaded(true);
             });
@@ -44,7 +47,7 @@ function Profile() {
 
     }, [status]);
 
-    const {firstname, lastname, email, phone, birthday, male, country, city} = profileData;
+    const {firstname, lastname, email, phone, birthday, male, country, city, mailerlist} = profileData;
 
     const onChangeInput = (e: any) => {
         const {name, value} = e.target
@@ -62,6 +65,10 @@ function Profile() {
 
     const onGenderChange = (male: boolean) => {
         setProfileData({...profileData, male: male})
+    }
+
+    const onChangeMailingList = (mailingList: boolean) => {
+        setProfileData({...profileData, mailerlist: mailingList})
     }
 
     function onSubmitForm(e) {
@@ -152,6 +159,18 @@ function Profile() {
                                         <input onChange={onChangeInput} value={city} name="city"
                                                className="border focus:outline focus:outline-sky-500 rounded-lg px-2 py-1"
                                                type="text"/>
+                                    </div>
+                                    <div className="flex flex-col gap-1">
+                                        <label className="relative inline-flex items-center cursor-pointer">
+                                        <input type="checkbox"
+                                               className="sr-only peer"
+                                               onChange={() => onChangeMailingList(mailerlist)}
+                                               checked={mailerlist}
+                                               />
+                                        <div
+                                            className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                                        <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Subscribed to mailing list</span>
+                                        </label>
                                     </div>
                                     <div className="flex justify-end md:col-start-2">
                                         <button type="submit"
