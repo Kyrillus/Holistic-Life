7643dfc745663690dcafb58747a9a3a63ce80a38
Sebastian Fürndraht <sebastian.fuerndraht@gmail.com>
2023-07-16 23:01:23 +0200

    IT-110 switched to flex-col

--- pages/jar-experiment.tsx
+++ pages/jar-experiment.tsx
@@ -9,17 +9,21 @@ function JarExperiment() {
     return (
         <div className="flex justify-center items-center pb-12 pt-4">
             <div className="w-2/3 max-w-4xl mx-auto">
-                <div className="grid gap-8 grid-cols-3 grid-rows-3 justify-items-center">
-                    <div></div>
-                    <div className="text-center"><h1>Jar Experiment</h1></div>
-                    <div className="justify-self-end">
-                        <Button colorScheme='blue' variant='outline'>show overview</Button>
+                <div className="flex flex-col gap-8 justify-items-center">
+                    <div className="flex flex-col gap-4 md:gap-0 md:flex-row justify-items-center place-content-between">
+                        <div className="w-0 md:w-40"></div>
+                        <div className="flex items-center justify-center">
+                            <h1>Jar Experiment</h1>
+                        </div>
+                        <div className="flex  md:gap-0 items-center justify-center">
+                            <Button className="md:w-40" colorScheme='blue' variant='outline'>show overview</Button>
+                        </div>
                     </div>
-                    <div className="col-span-3">
+                    <div>
                         <p className="text-center">Welcome to the start of your joruney to experience Lorem ipsum dolor sit amet,
                             consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt. </p>
                     </div>
-                    <div className="col-span-3">
+                    <div className="flex justify-center">
                         <Button colorScheme='blue'>get a random prompt</Button>
                     </div>
                 </div>
